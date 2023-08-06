1ebc32442d3a8d5ea97bdb83eef96823c046b9c4
rutvik-kumar-patel <rutvik16302@gmail.com>
2023-08-06 22:59:06 +0530

    File Added

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import  express  from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from 'cors';
+import dotenv from'dotenv';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+// const CONNCTION_URL = 'mongodb+srv://rutvik230200:rutvik230200@cluster0.br0zh6a.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNCTION_URL,{useNewUrlParser: true})
+.then(()=> app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
+.catch((error)=>console.log(error.message));
+
+
+
+
+

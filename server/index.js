4c0e36bcfc28d893e1e9d57503c7e4e9dfce36c3
rutvik-kumar-patel <rutvik16302@gmail.com>
2025-03-25 15:37:22 +0530

    add path in server in index.js

--- server/index.js
+++ server/index.js
@@ -22,10 +22,12 @@ app.use("/user", userRoutes);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "client/build")));
+const clientBuildPath = path.join(__dirname, "../client/build");
+
+app.use(express.static(clientBuildPath));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(clientBuildPath, "index.html"));
 });
 // --------------------------------
 
